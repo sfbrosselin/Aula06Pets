8be1d6e5582d45518e0b0b424446f1d6a8a0ef1b
sfbrosselin <brosselindev@gmail.com>
2026-03-25 20:30:35 -0300

    criacao da classe pet

---
+++ app/src/main/java/br/fecapads/projetoaula06/Pet.java
@@ -0,0 +1,18 @@
+package br.fecapads.projetoaula06;
+
+
+public class Pet {
+    String nome;
+    int idade;
+    String cor;
+    String raca;
+    String aniversario;
+
+    public Pet(String nome, int idade, String cor, String raca, String aniversario) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cor = cor;
+        this.raca = raca;
+        this.aniversario = aniversario;
+    }
+}
